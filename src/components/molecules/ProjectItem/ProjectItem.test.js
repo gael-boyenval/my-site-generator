f47d4cd77910e5adb8c5dca7de0ec2ab558e1b45
Gael BOYENVAL <gaelboyenval@icloud.com>
2018-04-05 17:14:10 +0200

    feat(mol/projectItem): create and add to SB

---
+++ src/components/molecules/ProjectItem/ProjectItem.test.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-undef */
+import React from 'react';
+import { MemoryRouter as Router } from 'react-router-dom';
+
+import ProjectItem from './ProjectItem';
+import mock from './ProjectItem.mock';
+
+const shallowWithRouter = (node) => shallow(<Router>{node}</Router>);
+
+describe('Component ProjectItem', () => {
+  it('should be render without crash', () => {
+    const component = shallowWithRouter(<ProjectItem num="01" {...mock} />);
+
+    expect(toJson(component)).toMatchSnapshot();
+  });
+});
