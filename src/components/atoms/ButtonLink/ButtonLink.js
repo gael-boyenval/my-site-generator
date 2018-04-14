eafa445d320371dd50e4d930f31d03da2da8acb8
Gael BOYENVAL <gaelboyenval@icloud.com>
2018-04-14 09:50:30 +0200

    update(atoms/buttonLink, atoms/icon): change arrow style and align on grid

--- src/components/atoms/ButtonLink/ButtonLink.js
+++ src/components/atoms/ButtonLink/ButtonLink.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import styled from 'styled-components';
 import { colors, unit } from 'Styles/tokens';
 import { rem } from 'Styles/utils';
 import { Margin } from 'Layout/Spaces';
@@ -14,9 +14,17 @@ const Button = TinyHeading.withComponent('a').extend`
   margin: 0;
 `;
 
+const IconWrapper = styled.div`
+  display: inline-block;
+  height: ${rem(unit / 2)};
+  margin-top: -${rem(unit / 2)};
+`;
+
 const ButtonLink = ({ children, ...rest }) => (
   <Button {...rest}>
-    <Icon name="arrowRight" size={rem(unit)} inlineBlock color={colors.electric} />
+    <IconWrapper>
+      <Icon name="arrowRight" size={rem(unit * 2)} color={colors.white.dark} />
+    </IconWrapper>
     <Margin left="s" inline>
       {children}
     </Margin>
